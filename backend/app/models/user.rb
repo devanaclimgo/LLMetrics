e31f4c6f541530f2f74ae4998db5d4fa5978c242
Ana <anaclimgo@gmail.com>
2026-04-13 14:57:56 -0300

    feat: add API key generation for User model on creation

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  before_create :generate_api_key
+
+  private
+
+  def generate_api_key
+    self.api_key = "sk_" + SecureRandom.hex(16)
+  end
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
