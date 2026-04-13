b8ae5b423c2633e24fb25994a1d17190ed409ebe
Ana <anaclimgo@gmail.com>
2026-04-13 15:15:10 -0300

    fix: update CORS configuration to restrict origins and allow all HTTP methods

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,9 +1,9 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-
+    origins "http://localhost:5173"
     resource "*",
       headers: :any,
-      methods: [:get, :post]
+      credentials: true,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
